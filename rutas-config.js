b878c7ff76347dd5d06974e539abe91fee883a8d
manuel-emergency <manuel-lopez-molina@hotmail.com>
2026-08-31 18:04:12 +0200

    Add files via upload

---
+++ rutas-config.js
@@ -0,0 +1,6 @@
+// Campings & Áreas - configuración de Rutas
+// Esta clave está limitada en Geoapify a campingsyareas.de / www.campingsyareas.de.
+window.RUTAS_CONFIG = Object.freeze({
+  GEOAPIFY_API_KEY: "036221e273c047bda13cbe55fac245c2",
+  MAP_STYLE: "osm-bright"
+});
