25a3293d85e795ca50de9af74b29517dd4bc0873
manuel-emergency <manuel-lopez-molina@hotmail.com>
2026-09-18 10:08:45 +0200

    Rutas: elimina import duplicado y literal inválido del entry point

--- app/trip/rutas-entry.js
+++ app/trip/rutas-entry.js
@@ -3,7 +3,6 @@ import { prepareTripFromCurrentForm, createProductionGuideAdapter } from "./cont
 import { renderRouteMap } from "../ui/route-map.js";
 import { renderLongFormGuide } from "../ui/route-guide.js";
 import { renderRouteActions } from "../ui/route-actions.js";
-import { renderRouteActions } from "../ui/route-actions.js";
 
 let currentStep = 1;
 let childAgeCount = 0;
